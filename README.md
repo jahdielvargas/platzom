b3d5d208272ffedf728c2a19cadc92ca802504d6
jahdielvargas <jahdielvargasmusic@gmail.com>
2018-02-19 00:52:50 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Platzom
+Platzom es un idioma inventado para el curso de
+[Fundamentos de Javascript](https://www.platzi.com/js)
+en [Platzi](https://www.platzi.com), el mejor lugar de
+educación online.
+
+## Descripción del idioma
+- si la palabra termina con "ar", se le quitan las dos ultimas letras
+- si la palabra termina con "ar", se le quitan las dos ultimas letras
+- si la palabra termina con "ar", se le quitan las dos ultimas letras
+
+## Instalación
+```
+npm  install Platzom
+```
+## Uso
+```
+import platzom from 'platzom'
+platzom('programar') // program
+```
+## Créditos
+- [Jahdiel Vargas](https://platzi.com/@jahdielvargas/)
+
+## Licencias
+[MIT](https://opensource.org/licenses/MIT)
---
+++ test/test.js
@@ -0,0 +1,23 @@
+const expect = require('chai').expect
+const platzom = require('..').default
+
+describe('·platzom', function (){
+  it('Si la palabra termina con "ar", se le quitan esas dos letras', function(){
+    const translation = platzom('programar')
+    expect(translation).to.equal('program')
+  })
+  it('Si la palabra inicia con "z", se le añade "pe" al final', function(){
+    const translation = platzom('zorro')
+    const translation2 = platzom('zarpar')
+    expect(translation).to.equal('zorrope')
+    expect(translation2).to.equal('zarppe')
+  })
+  it('Si la palabra tiene 10 o mas letras, se debe partir en dos por la mitad y unir con un guion', function(){
+    const translation = platzom('abecedario')
+    expect(translation).to.equal('abece-dario')
+  })
+  it('Si la palabra es palindromo, obvia todas las reglas anteriores y se delvuelve intercalada por mayusculas y minusculas', function(){
+    const translation = platzom('sometemos')
+    expect(translation).to.equal('SoMeTeMoS')
+  })
+})
